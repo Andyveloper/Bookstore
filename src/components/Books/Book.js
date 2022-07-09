5470f31522e00e0afd486f6c8e963d8b76d2705d
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-07-09 00:32:58 -0500

    Add props to Book component

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,13 +1,19 @@
 import { CircularProgressbar } from 'react-circular-progressbar';
+import { PropTypes } from 'prop-types';
 
-const Book = () => {
-  const percentage = 50;
+const Book = (props) => {
+  const {
+    title,
+    author,
+    chapter,
+    percentage,
+  } = props;
 
   return (
     <div className="book-list-container">
       <div className="book-list-info">
-        <h2 className="book-list__book-title">Title Goes Here</h2>
-        <p className="book-list__book-author">Author Goes Here</p>
+        <h2 className="book-list__book-title">{title || 'Title Goes Here'}</h2>
+        <p className="book-list__book-author">{author || 'Author Goes Here'}</p>
         <div className="actions-container">
           <ul className="actions-list">
             <li className="action-list__item"><a href="/">Comments</a></li>
@@ -20,13 +26,20 @@ const Book = () => {
         </div>
         <div className="current-progress">
           <h2>CURRENT CHAPTER</h2>
-          <p>Chapter 17</p>
+          <p>{chapter || 'Chapter 17'}</p>
           <button type="submit">UPDATE PROGRESS</button>
         </div>
       </div>
 
     </div>
   );
+};
+
+Book.propTypes = {
+  author: PropTypes.string.isRequired,
+  chapter: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
 };
 
 export default Book;
